ccb1c9d6da65a21889f05bdd1eeacf35937305ad
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-04-07 09:53:56 +0530

    main function. instead of writing credentials, we can refer back to the variables in config files

--- scraper/default.py
+++ scraper/default.py
@@ -5,3 +5,13 @@ import praw
 from config import *
 from image_resolver import *
 from task_downloader import *
+
+if __name__ == '__main__':
+    reddit = praw.Reddit(client_id=CLIENT_ID,
+                         client_secret=CLIENT_SECRET,
+                         password=PASSWORD,
+                         user_agent=USER_AGENT,
+                         username=USERNAME)
+
+    last_id = ''
+    download_data = []
