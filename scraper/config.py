0eab24279e83112417cba30fa7630ba1c2128706
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-04-07 09:51:18 +0530

    Added the required variables through config.py

---
+++ scraper/config.py
@@ -0,0 +1,13 @@
+SUBREDDIT = 'funny'
+
+USERNAME = 'DeeprajPandey'
+PASSWORD = '9776845551'
+CLIENT_SECRET = 'E1YGUxtehUxgpvmaneaD4EGD0Jc'
+CLIENT_ID = 'YYX_hHa_ReWsRw'
+PLATFORM = 'python'
+APP_ID = 'Scraper'
+VERSION = 'v0.0.1'
+LIMIT = 200
+DATA_DIR = './data'
+
+USER_AGENT = '{platform}:{app_id}:{version} (by /u/{username})'.format(platform=PLATFORM, app_id=APP_ID, version=VERSION, username=USERNAME)
