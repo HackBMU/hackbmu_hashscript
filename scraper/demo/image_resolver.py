6154ebd5a0318e673c1ea253ac9f72f965984e78
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-04-07 10:18:07 +0530

    photos on imgur if they are *not*galleries and are only jpg files

--- scraper/demo/image_resolver.py
+++ scraper/demo/image_resolver.py
@@ -2,5 +2,8 @@ def parse_url(url):
 	parsed = None
 	if url.endswith('.jpg'):
 		parsed = url
+	elif 'imgur.com' in url:
+		if '/a/' not in url and '/gallery/' not in url:
+			parsed = 'http://i.imgur.com/{id}.jpg'.format(id=url.split('/')[-1])
 
 	return parsed
