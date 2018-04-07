2f2d62a7be2f138c9e8a47f9df7f02cc2a8fe79f
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-04-07 10:20:24 +0530

    Harder than I think it would be.

--- scraper/demo/task_downlader.py
+++ scraper/demo/task_downlader.py
@@ -2,3 +2,8 @@ import asyncio
 import os
 from aiohttp import ClientSession
 from config import *
+
+async def download_image(url, id):
+    if os.path.isfile(os.path.join(DATA_DIR, id + '.jpg')):
+        return
+    async with ClientSession() as session:
