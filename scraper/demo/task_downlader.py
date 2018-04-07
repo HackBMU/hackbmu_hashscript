f317f236132fd20c27cec2f91ba9a8bb96989ad5
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-04-07 10:21:13 +0530

    Save the image files. Name them based on id.

--- scraper/demo/task_downlader.py
+++ scraper/demo/task_downlader.py
@@ -7,3 +7,7 @@ async def download_image(url, id):
     if os.path.isfile(os.path.join(DATA_DIR, id + '.jpg')):
         return
     async with ClientSession() as session:
+        async with session.get(url) as response:
+            response = await response.read()
+            with open(os.path.join(DATA_DIR, id + '.jpg'), 'wb') as f:
+                f.write(response)
