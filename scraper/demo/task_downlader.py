9bdfcc4bdac8dd7efb22ebd5ff450f9cd0d8d00a
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2018-04-07 10:21:55 +0530

    I think I am done?

--- scraper/demo/task_downlader.py
+++ scraper/demo/task_downlader.py
@@ -16,3 +16,8 @@ def download_images(download_data):
     loop = asyncio.get_event_loop()
 
     tasks = []
+    for dl in download_data:
+        task = asyncio.ensure_future(download_image(dl[0], dl[1]))
+        tasks.append(task)
+
+    loop.run_until_complete(asyncio.wait(tasks))
